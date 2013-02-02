f6a170a38159ff2b4e7495ae0ec203c978736baf
Andrew Jordan <acjordan2gmail.com>
2013-02-01 21:30:18 -0600

    Fixed logout errors when PHPSSID was not set

--- logout.php
+++ logout.php
@@ -4,9 +4,11 @@ setcookie($name=AUTH_KEY1, $value='', $expire=1, $path="/",
 			$path=DOMAIN, $secure=USE_SSL, $httponly=TRUE);
 setcookie($name=AUTH_KEY2, $value='', $expire=1, $path="/",           
                         $path=DOMAIN, $secure=USE_SSL, $httponly=TRUE);
-$params = session_get_cookie_params();
-setcookie($name=session_name(), $value='', $expire=1, $path="/", 
-			$domain=DOMAIN, $secure=USE_SSL, $httponly=TRUE);
-session_destroy();
+if(isset($_COOKIE[session_name()])){                       
+	$params = session_get_cookie_params();
+	setcookie($name=session_name(), $value='', $expire=1, $path="/", 
+				$domain=DOMAIN, $secure=USE_SSL, $httponly=TRUE);
+	session_destroy();
+}
 header("Location: /index.php");
 ?>
