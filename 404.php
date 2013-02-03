d4b4e484fadf1ba6ab5b77382a3d637bd0ebf223
Andrew Jordan <acjordan2gmail.com>
2013-02-02 19:19:59 -0600

    Added link search. Changed dir structure, added install instructions

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("includes/init.php");
-if($auth == TRUE){
-	header('HTTP/1.1 404 Not Found');
-	$display = "404.tpl";
-	require_once("includes/deinit.php");
-}else
-	header("Location: /?r=".rawurlencode($_SERVER['REQUEST_URI']));
-?>
--- templates/default/footer.tpl
+++
@@ -1,3 +0,0 @@
-			<small>
-				<b>Time Taken:</b> {$load_time}s 
-			</small>
