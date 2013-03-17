ba57f8a189f82d1e49d3bb9ae5a67c9acee17f3d
Andrew Jordan <acjordan2gmail.com>
2013-03-16 20:19:25 -0500

    Fixed error messages

--- www/logout.php
+++ www/logout.php
@@ -6,9 +6,9 @@ setcookie($name=AUTH_KEY2, $value='', $expire=1, $path="/",
                         $path=DOMAIN, $secure=USE_SSL, $httponly=TRUE);
 if(isset($_COOKIE[session_name()])){                       
 	$params = session_get_cookie_params();
+	@session_destroy();
 	setcookie($name=session_name(), $value='', $expire=1, $path="/", 
 				$domain=DOMAIN, $secure=USE_SSL, $httponly=TRUE);
-	session_destroy();
 }
 header("Location: /index.php");
 ?>
