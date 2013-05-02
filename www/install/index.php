23e5c93a45747889f60c0250d49593f07eb758fc
Andrew Jordan <acjordan2@gmail.com>
2013-05-02 00:50:13 -0500

    Added check for magic quotes

--- www/install/index.php
+++ www/install/index.php
@@ -49,6 +49,8 @@ $curl_installed = function_exists('curl_version');
 @unlink(TEMPLATE_COMPILE."/test.txt");
 @unlink($root_path."/includes/test.txt");
 @unlink($root_path."/install/test.txt");
+
+$magic_quotes = get_magic_quotes_gpc();
 ?>
 <html>
 <head>
@@ -92,12 +94,18 @@ $curl_installed = function_exists('curl_version');
 			<td><?php if($include_perms) print "Yes"; else print "No" ?></td>
 			<td style="background-color: <?php if($include_perms) print "green"; else print "red"; ?>"><?php if($include_perms) print "OK";else{print "Failed"; $failed=TRUE;}?></td>
 		</tr>
+		<tr>
+			<td>Magic Quotes Disabled</td>
+			<td><?php if(!$magic_quotes) print "Yes"; else print "No" ?></td>
+			<td style="background-color: <?php if(!$magic_quotes) print "green"; else print "red"; ?>"><?php if(!$magic_quotes) print "OK";else{print "Failed"; $failed=TRUE;}?></td>
+		</tr>
 	</table>
 	<br />
 	<br />
 	<?php if(!$template_perms) print "run chmod 777 ".TEMPLATE_COMPILE."<br />";
 		if(!$install_perms) print "run chmod 777 ".$root_path."/install<br />";
-		if(!$include_perms) print "run chmod 777 ".$root_path."/includes";
+		if(!$include_perms) print "run chmod 777 ".$root_path."/includes<br />";
+		if($magic_quotes) print "Magic quotes can cause unexpected bahavior and should be disabled, instructions can be found <a href=\"http://php.net/manual/en/security.magicquotes.disabling.php\" target=\"_blank\">here</a>";
 		  if(!$failed){
 	 ?>
 		<form action="step2.php" method="get">
