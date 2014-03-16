1435052d7552c1473cfbde076b43fdffd7dd01c9
Andrew Jordan <acjordan2@gmail.com>
2014-03-16 12:27:02 -0700

    Update Install.inc.php

--- www/install/Install.inc.php
+++ www/install/Install.inc.php
@@ -2,10 +2,11 @@
 $install = true;
 
 require_once("../includes/functions.php");
-require_once("../includes/Config.ini.php");
-require_once("../includes/Override.inc.php");
 
 $root_path = get_root_path();
+
+require_once("../includes/Config.ini.php");
+require_once("../includes/Override.inc.php");
 
 if(file_exists($root_path."/install/install.lock"))
 	die("Remove the install.lock file from the install directory to continue");
