01ba13b4ba4df6baf5d654830013c84d775c8d3a
Andrew Jordan <acjordan2@gmail.com>
2014-03-16 12:25:43 -0700

    Added $root_path variable to install script.

--- www/install/Install.inc.php
+++ www/install/Install.inc.php
@@ -5,6 +5,8 @@ require_once("../includes/functions.php");
 require_once("../includes/Config.ini.php");
 require_once("../includes/Override.inc.php");
 
+$root_path = get_root_path();
+
 if(file_exists($root_path."/install/install.lock"))
 	die("Remove the install.lock file from the install directory to continue");
 ?>
