4b3d9bd6afabe984d2e90d6d69e7f7ba0790e819
Andrew Jordan <acjordan2@gmail.com>
2014-03-17 13:35:49 -0700

    Added missing }

--- www/install/Install.inc.php
+++ www/install/Install.inc.php
@@ -7,6 +7,7 @@ $root_path = get_root_path();
 
 if (file_exists($root_path."/includes/Database.ini.php")) {
     require_once("../includes/Database.ini.php");
+}
 
 require_once("../includes/Config.ini.php");
 require_once("../includes/Override.inc.php");
