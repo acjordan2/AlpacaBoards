b914df056f22663a0e920dc5c1acc5580d718ce6
Andrew Jordan <acjordan2@gmail.com>
2014-08-30 15:57:37 -0700

    Added comments

--- www/includes/Message.class.php
+++ www/includes/Message.class.php
@@ -24,35 +24,91 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-Class Message {
+Class Message
+{
     
+    /**
+     * Database connection object
+     * @var db_connection
+     */
     private $_pdo_conn;
 
+    /**
+     * Site object
+     * @var Site
+     */
     private $_site;
 
+    /**
+     * Message ID
+     * @var integer
+     */
     private $_message_id;
 
+    /**
+     * Message text
+     * @var string
+     */
     private $_message;
 
+    /**
+     * Topic ID for the message
+     * @var integer
+     */
     private $_topic_id;
 
+    /**
+     * Message revision number
+     * @var integer
+     */
     private $_revision_no;
 
+    /**
+     * User ID of the message poster
+     * @var [type]
+     */
     private $_user_id;
 
+    /**
+     * Username of the message poster
+     * @var string
+     */
     private $_username;
 
+    /**
+     * Title of the parent object (ie link, topic, etc)
+     * @var string
+     */
     private $_title;
 
+    /**
+     * State of the message, 0 for existing, 1 for deleted, 
+     * 2 for deleted by moderator. 
+     * @var integer
+     */
     private $_state;
 
-    public function __construct($site, $message_id, $revision_no = 0)
+    /**
+     * Create a new Message Object
+     * 
+     * @param Site    $site        Site object to get globals variables
+     * @param integer $message_id  ID of the message to retrieve
+     * @param integer $revision_no Revision number of the message
+     */
+    public function __construct(Site $site, $message_id, $revision_no = 0)
     {
         $this->_pdo_conn = ConnectionFactory::getInstance()->getConnection();
         $this->_site = $site;
         $this->_loadMessage($message_id, $revision_no);
     }
 
+    /**
+     * Load message data
+     * @param  integer   $message_id  Message ID
+     * @param  integer   $revision_no Revision number
+     * @throws exception              If the provided Message ID and revision number do not exist
+     * @return void              
+     */
     private function _loadMessage($message_id, $revision_no)
     {
         $sql = "SELECT Messages.topic_id, Messages.message_id, Messages.revision_no, Messages.user_id, 
@@ -87,7 +143,7 @@ Class Message {
             $this->_topic_id = $results['topic_id'];
             $this->_revision_no = $results['revision_no'];
 
-            
+            // Replace message text if the message has been deleted
             if ($this->_state == 0) {
                 $this->_message = $results['message'];
             } elseif ($this->_state == 1) {
@@ -101,6 +157,13 @@ Class Message {
         }
     }
 
+    /**
+     * Delete a message from a topic
+     * @param  integer $action       1 if the creator is deleting the message, 2 if its a moderator
+     * @param  integer $moderator_id ID of the moderator deleting the message
+     * @param  string  $reason       Reason for deleting the message if a moderator
+     * @return void               
+     */
     public function delete($action, $moderator_id = null, $reason = null)
     {
         $sql_delete = "UPDATE Messages SET deleted = :deleted WHERE message_id = ".$this->_message_id;
@@ -123,6 +186,10 @@ Class Message {
         }
     }
 
+    /**
+     * Get the revision history of a message
+     * @return array Message revision history
+     */
     public function getRevisions()
     {
         $table_name = "Messages";
@@ -136,6 +203,11 @@ Class Message {
         return $results;
     }
 
+    /**
+     * Get the message content
+     * @param  boolean $quote True if the content will be used as a quote
+     * @return string         The message
+     */
     public function getMessage($quote = false)
     {
         if ($quote == true) {
@@ -159,31 +231,64 @@ Class Message {
         return $message;
     }
 
+    /**
+     * Return the message state, 0 if the message still exists,
+     * 1 if it has been deleted, 2 if it has been deleted by a 
+     * moderator
+     * 
+     * @return integer The message state
+     */
     public function getState()
     {
         return $this->_state;
     }
 
+    /**
+     * Title of the parent object for which the message belongs
+     * (ie topic, link, etc)
+     * 
+     * @return string Title
+     */
     public function getTitle()
     {
         return $this->_title;
     }
 
+    /**
+     * Get the username of the message poster
+     * 
+     * @return string Username
+     */
     public function getUsername()
     {
         return $this->_username;
     }
 
+    /**
+     * Get the user ID of the message poster 
+     * 
+     * @return integer User ID
+     */
     public function getUserId()
     {
         return $this->_user_id;
     }
 
+    /**
+     * Get the timestamp of when the message was posted
+     * 
+     * @return integer Unix timestamp
+     */
     public function getPosted()
     {
         return $this->_posted;
     }
 
+    /**
+     * Get the current revision number for a given message
+     * 
+     * @return integer Revision number
+     */
     public function getRevisionId()
     {
         return $this->_revision_no;
