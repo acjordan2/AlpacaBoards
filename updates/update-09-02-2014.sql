f87ea3361da85d0fa54d5d831a614fba611a45cb
Andrew Jordan <acjordan2@gmail.com>
2014-10-09 22:23:38 -0700

    Removed foreign key constraint

--- updates/update-09-02-2014.sql
+++ updates/update-09-02-2014.sql
@@ -1,2 +1,3 @@
 ALTER TABLE  `Messages` ADD  `type` INT( 1 ) NOT NULL ;
 ALTER TABLE  `Messages` ADD  `link_id` INT( 11 ) NULL AFTER `topic_id` ;
+ALTER TABLE  `Messages` DROP FOREIGN KEY  `Messages_ibfk_1`;
