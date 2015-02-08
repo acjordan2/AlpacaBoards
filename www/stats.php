60eab8eff6bad74e5ef0e4181f2cc93f9eb5c1ee
Andrew Jordan <acjordan2@gmail.com>
2015-02-07 23:00:34 -0800

    Added more stats

--- www/stats.php
+++ www/stats.php
@@ -37,16 +37,34 @@ if ($auth == true) {
     $sql_get_links = "SELECT COUNT(link_id) as count from Links";
     $query_links = $db->query($sql_get_links);
     $link_num = $query_links->fetch();
+   
+    // Get number of topics 
+    $sql_get_topics = "SELECT COUNT(topic_id) as count FROM Topics";
+    $query_topics = $db->query($sql_get_topics);
+    $topic_num = $query_topics->fetch();
 
     // Get number of Messages
     $sql_get_messages = "SELECT COUNT(message_id) FROM Messages";
     $query_messages = $db->query($sql_get_messages);
     $message_num = $query_messages->fetch();
 
+    // Get number of images
+    $sql_get_images = "SELECT COUNT(uploadlog_id) as count FROM UploadLog";
+    $query_images = $db->query($sql_get_images);
+    $image_num = $query_images->fetch();
+    
+    // Get number of unique images
+    $sql_get_images_unique = "SELECT COUNT(image_id) as count FROM UploadedImages";
+    $query_images_unique = $db->query($sql_get_images_unique);
+    $image_num_unique = $query_images_unique->fetch();
+
     // Assign template variables
     $smarty->assign("user_num", $user_num[0]);
     $smarty->assign("links_num", $link_num[0]);
+    $smarty->assign("topic_num", $topic_num[0]);
     $smarty->assign("message_num", $message_num[0]);
+    $smarty->assign("image_num", $image_num[0]);
+    $smarty->assign("image_unique_num", $image_num_unique[0]);
 
     // Set page template
     $display = "stats.tpl";
--- www/templates/default/stats.tpl
+++ www/templates/default/stats.tpl
@@ -4,7 +4,10 @@
 	<br />
 	<b>Total Users:</b> {$user_num}<br />
 	<b>Total Links:</b> {$links_num}<br />
-	<b>Total Messags:</b> {$message_num}<br />
+    <b>Total Topics:</b> {$topic_num}<br />
+	<b>Total Messages:</b> {$message_num}<br />
+    <b>Total Images Uploaded:</b> {$image_num}<br />
+    <b>Total Unique Images Uploaded:</b> {$image_unique_num}
 	<br />
 	<br />
 {include file="footer.tpl"}
